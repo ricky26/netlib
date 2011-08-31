15b6d9529834bb73fb0575aff61c23dc83af5979
Ricky Taylor <rickytaylor26@gmail.com>
2011-08-31 20:44:15 +0100

    Added data class.

--- include/netlib/data.h
+++ include/netlib/data.h
@@ -1,29 +1,30 @@
 #include "netlib.h"
+#include "owned_ptr.h"
 
 #pragma once
 
 namespace netlib
 {
-	class NETLIB_API data
+	class data
 	{
 	public:
-		NETLIB_INLINE data() : mData(NULL), mSize(0) {}
+		NETLIB_INLINE data() : mSize(0) {}
 		NETLIB_INLINE data(void *_ptr, size_t _sz): mData(_ptr), mSize(_sz) {}
 		
-		void set(data &_d);
-		void set(void *_ptr, size_t _sz);
-		NETLIB_INLINE void *take();
+		NETLIB_INLINE void set(data &_d) { set(_d.ptr(), _d.size()); }
+		NETLIB_INLINE void set(void *_ptr, size_t _sz) { mData = _ptr; mSize = _sz; }
+		NETLIB_INLINE void *take() { return mData.take(); }
 
-		NETLIB_INLINE void *ptr() const { return mData; }
+		NETLIB_INLINE void *ptr() const { return mData.get(); }
 		NETLIB_INLINE size_t size() const { return mSize; }
 
 		template<typename T>
-		NETLIB_INLINE T *ptr() const { return static_cast<T*>(mData); }
+		NETLIB_INLINE T *ptr() const { return static_cast<T*>(mData.get()); }
 
-		NETLIB_INLINE data &operator =(data &_d) { set(_d); }
+		NETLIB_INLINE data &operator =(data &_d) { set(_d); return *this; }
 
 	private:
-		void *mData;
+		owned_ptr<> mData;
 		size_t mSize;
 	};
 }
